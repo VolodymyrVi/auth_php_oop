6674d755398ef5cb83992df7bbfff77beaf0b320
VolodymyrVi <sqlphpjs@gmail.com>
2021-04-06 17:55:45 +0300

    added anotations

--- src/Authorization.php
+++ src/Authorization.php
@@ -6,14 +6,27 @@ namespace App;
 class Authorization
 {
     private Database $database;
-
+    /**
+     * Undocumented function
+     *
+     * @param Database $database
+     */
     public function __construct(Database $database)
     {
         $this->database = $database;
     }
-
+    /**
+     * Authoriztion function register
+     *
+     * @param array $data
+     * @return boolean
+     * @throws AuthorizationException
+     */
     public function register(array $data): bool
     {
+        if (empty($data['username'])){
+            throw new AuthorizationException('The username should not be empty');
+        }
         return true;
     }
 }
